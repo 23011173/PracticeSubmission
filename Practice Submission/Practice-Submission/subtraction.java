b8960747067083bf08c33f5fb00575554efc1e24
23011173 <163440406+23011173@users.noreply.github.com>
2024-03-16 11:53:57 -0700

    Add files via upload

---
+++ Practice Submission/Practice-Submission/subtraction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class subtraction
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a and b");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a-b;
+        System.out.println("The value of c="+c);
+    }
+}
